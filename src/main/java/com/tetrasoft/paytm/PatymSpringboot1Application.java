dcc64811802ef2e0f8f57df52718152417cf755a
Akshay Kumar Phatale <tsipl1295@tetrasoft.in>
2019-09-13 18:27:12 +0530

    First Commit

---
+++ src/main/java/com/tetrasoft/paytm/PatymSpringboot1Application.java
@@ -0,0 +1,13 @@
+package com.tetrasoft.paytm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatymSpringboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatymSpringboot1Application.class, args);
+	}
+
+}
